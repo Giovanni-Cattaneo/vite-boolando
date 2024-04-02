64d9d5ba233a923e852f82b508416ecdffc79d4d
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-04-02 11:28:35 +0200

    minor edito forgotten to commit

--- src/components/appCard.vue
+++ src/components/appCard.vue
@@ -4,7 +4,7 @@ import { state } from "../state.js"
 
 export default {
     name: 'appCard',
-    props: ['brand', 'image', 'price', 'altPrice', 'model', 'altImage', 'originalimage', 'taglie'],
+    props: ['brand', 'image', 'price', 'discount', 'model', 'altImage', 'originalimage', 'taglie'],
     data() {
         return {
             reveal: false,
@@ -41,7 +41,7 @@ export default {
                 <!-- utiizziamo le props coem variabili per l'inserimento nelle card -->
                 <h5>{{ brand }}</h5>
                 <span class="price">{{ price }}$</span>
-                <span class="realPrice">{{ (price / 2).toFixed(2) }}$</span>
+                <span class="realPrice">{{ (price * discount).toFixed(2) }}$</span>
 
             </div>
 
@@ -55,7 +55,7 @@ export default {
                 <p>Brand: {{ brand }}</p>
                 <p>Modello: {{ model }}</p>
                 <p>Prezzo Intero: {{ price }}$</p>
-                <p>Prezzo scontato {{ (price / 2).toFixed(2) }}$</p>
+                <p>Prezzo scontato {{ (price * discount).toFixed(2) }}$</p>
                 <p>Taglie disponibili: {{ taglie }}</p>
                 <p>Per informazioni sulla spedizioni rifarsi alle nostre condizioni di utilizzo e spedizione
                     internazionali
